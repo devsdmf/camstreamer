57ce4464b21c19231ea486b46e17b9481c5a5b2e
devsdmf <devsdmf@gmail.com>
2020-04-02 18:25:57 -0300

    Initial Commit

---
+++ camstreamer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  camstreamer
+//
+//  Created by Lucas Mendes on 02/04/20.
+//  Copyright © 2020 devSDMF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
