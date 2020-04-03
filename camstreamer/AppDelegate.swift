85e62ec8d586f5ed031c6af49e79f104bc29993b
devsdmf <devsdmf@gmail.com>
2020-04-03 16:43:52 -0300

    First commit

--- camstreamer/AppDelegate.swift
+++ camstreamer/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.isIdleTimerDisabled = true
         return true
     }
 
@@ -32,6 +33,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
--- camstreamer/ViewController.swift
+++ camstreamer/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class CustomUIImagePickerController: UIImagePickerController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        modalPresentationStyle = .fullScreen
+    }
+    
+    override public var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override public var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeRight
+    }
+    
+    override public var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return .landscapeRight
+    }
+    
+}
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        modalPresentationStyle = .fullScreen
         // Do any additional setup after loading the view.
     }
+    
+    override public var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override public var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeRight
+    }
+    
+    override public var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return .landscapeRight
+    }
 
-
+    @IBAction func chooseFromCamera() {
+        let picker: UIImagePickerController = CustomUIImagePickerController()
+        
+        picker.delegate = self;
+        picker.sourceType = .camera
+        
+        picker.setNavigationBarHidden(true, animated: false)
+        picker.mediaTypes = [kUTTypeMovie as String, kUTTypeImage as String]
+        picker.cameraCaptureMode = UIImagePickerController.CameraCaptureMode.video
+        picker.videoQuality = UIImagePickerController.QualityType.typeHigh
+        picker.showsCameraControls = false
+        
+        picker.cameraViewTransform.ty += 100;
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
 }
 
